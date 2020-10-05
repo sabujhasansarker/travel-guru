dd37627b0e9625595fd9309f5f7a6705db3402b4
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-05 14:11:18 +0600

    slider #1

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,8 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import TravelContext from "../../context/travel/TravelContext";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const { data } = useContext(TravelContext);
+  const [trigger, setTrigger] = useState(0);
+  const mainData = data.mainData && data.mainData[0];
+  const triggerHeight = data.mainData && data.mainData.length - 1;
   return (
     <div
       className="home"
@@ -12,7 +16,59 @@ const Home = () => {
         })`,
       }}
     >
-      <section className="banner"></section>
+      <section className="banner">
+        <div className="container">
+          <div className="banner-left">
+            <h1 className="heading">{mainData && mainData.name}</h1>
+            <p className="text">{mainData && mainData.descriptionHome}</p>
+            <Link to="/booking" className="btn">
+              Booking
+            </Link>
+          </div>
+          <div className="banner-right">
+            <div className="slider">
+              {data.mainData &&
+                data.mainData.map((data, index) => (
+                  <div
+                    key={index}
+                    className={`slider-single ${
+                      trigger == index ? "slider-active" : ""
+                    } ${trigger > index ? "slider-none" : ""} ${trigger}`}
+                    style={{ backgroundImage: `url(${data.img})` }}
+                  >
+                    <h2 className="heading"> {data && data.name}</h2>
+                  </div>
+                ))}
+            </div>
+            <div className="trigger mt-2">
+              <div
+                className={`trigger-left ${trigger === 0 ? "disable" : ""}`}
+                onClick={() => setTrigger(trigger <= 0 ? 0 : trigger - 1)}
+              >
+                <img
+                  src={data.essentialData && data.essentialData.arrow}
+                  alt=""
+                />
+              </div>
+              <div
+                className={`trigger-right ml-1 ${
+                  triggerHeight === trigger ? "disable" : ""
+                }`}
+                onClick={() =>
+                  setTrigger(
+                    trigger >= triggerHeight ? triggerHeight : trigger + 1
+                  )
+                }
+              >
+                <img
+                  src={data.essentialData && data.essentialData.arrow}
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
